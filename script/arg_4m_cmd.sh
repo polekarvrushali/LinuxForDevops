398d329d998968de786e6004ab4491b97bea5c51
Vrushali Polekar <polekarvrushali@gmail.com>
2023-05-15 15:01:53 +0200

    todays shell scripts

---
+++ script/arg_4m_cmd.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Total arguments = $#"
+
+echo "1 st argument=$1"
+echo "2 nd argument=$2"
---
+++ script/substr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+Str="Learn Linux from LinuxHint"
+subStr=${Str:11:14}
+echo $subStr
